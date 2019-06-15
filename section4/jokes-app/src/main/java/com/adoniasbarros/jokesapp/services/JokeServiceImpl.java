c8889fb144cb89f195415a01cc097d33ac67ced1
adoniasvsbarros <adoniasvicente@hotmail.com>
2019-06-14 21:32:59 -0300

    chuck norris app java based configuration

--- section4/jokes-app/src/main/java/com/adoniasbarros/jokesapp/services/JokeServiceImpl.java
+++ section4/jokes-app/src/main/java/com/adoniasbarros/jokesapp/services/JokeServiceImpl.java
@@ -1,5 +1,6 @@
 package com.adoniasbarros.jokesapp.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
@@ -9,8 +10,9 @@ public class JokeServiceImpl implements JokeService {
 
 	private final ChuckNorrisQuotes chuckNorrisQuotes;
 
-	public JokeServiceImpl() {
-		this.chuckNorrisQuotes = new ChuckNorrisQuotes();
+	@Autowired
+	public JokeServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
+		this.chuckNorrisQuotes = chuckNorrisQuotes;
 	}
 
 	@Override
