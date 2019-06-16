756d2d755e53c93b55e7390eb7a8ded6e2692a1e
adoniasvsbarros <adoniasvicente@hotmail.com>
2019-06-15 22:18:04 -0300

    adding multiple property files

--- section3/spring5-di-demo-di-assignment/src/main/java/guru/springframework/DiDemoApplication.java
+++ section3/spring5-di-demo-di-assignment/src/main/java/guru/springframework/DiDemoApplication.java
@@ -5,6 +5,7 @@ import guru.springframework.controllers.GetterInjectedController;
 import guru.springframework.controllers.MyController;
 import guru.springframework.controllers.PropertyInjectedController;
 import guru.springframework.examplebeans.FakeDataSource;
+import guru.springframework.examplebeans.FakeJmsBroker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -23,6 +24,14 @@ public class DiDemoApplication {
 		FakeDataSource fakeDataSource = (FakeDataSource) ctx.getBean(FakeDataSource.class);
 		
 		System.out.println(fakeDataSource.getUser());
+		System.out.println(fakeDataSource.getPassword());
+		System.out.println(fakeDataSource.getUrl());
+		
+		FakeJmsBroker fakeJmsBroker = (FakeJmsBroker) ctx.getBean(FakeJmsBroker.class);
+		System.out.println(fakeJmsBroker.getUsername());
+		System.out.println(fakeJmsBroker.getPassword());
+		System.out.println(fakeJmsBroker.getUrl());
+		
 		/*
 		System.out.println(controller.hello());
 		System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
--- section3/spring5-di-demo-di-assignment/src/main/java/guru/springframework/config/PropertyConfig.java
+++ section3/spring5-di-demo-di-assignment/src/main/java/guru/springframework/config/PropertyConfig.java
@@ -5,13 +5,20 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.core.env.Environment;
 
 import guru.springframework.examplebeans.FakeDataSource;
+import guru.springframework.examplebeans.FakeJmsBroker;
 
 @Configuration
-@PropertySource("classpath:datasource.properties")
+//@PropertySource({"classpath:datasource.properties", "classpath:jms.properties"})	
+@PropertySources({
+		@PropertySource("classpath:datasource.properties"),
+		@PropertySource("classpath:jms.properties")
+		
+})
 public class PropertyConfig {
 
 	@Autowired
@@ -26,6 +33,15 @@ public class PropertyConfig {
 	@Value("${guru.dbUrl}")
 	String url;
 	
+	@Value("${guru.jms.username}")
+	String jmsUsername;
+	
+	@Value("${guru.jms.password}")
+	String jmsPassword;
+	
+	@Value("${guru.jms.url}")
+	String jmsUrl;
+	
 	@Bean
 	public FakeDataSource fakeDataSource() {
 		FakeDataSource fakeDataSource = new FakeDataSource();
@@ -33,6 +49,15 @@ public class PropertyConfig {
 		fakeDataSource.setPassword(password);
 		fakeDataSource.setUrl(url);
 		return fakeDataSource;
+	}
+	
+	@Bean
+	public FakeJmsBroker FakeJmsBroker() {
+		FakeJmsBroker fakeJmsBroker = new FakeJmsBroker();
+		fakeJmsBroker.setUsername(jmsUsername);
+		fakeJmsBroker.setPassword(jmsPassword);
+		fakeJmsBroker.setUrl(jmsUrl);
+		return fakeJmsBroker;
 	}
 	
 	@Bean
