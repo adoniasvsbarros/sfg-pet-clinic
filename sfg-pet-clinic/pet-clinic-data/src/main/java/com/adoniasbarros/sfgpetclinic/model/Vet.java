160bb5aff1fbb22d25aa125851469c3a1bee9fc9
adoniasvsbarros <adoniasvicente@hotmail.com>
2019-06-16 20:05:41 -0300

    Added specialities, added to vet, closes #27

--- sfg-pet-clinic/pet-clinic-data/src/main/java/com/adoniasbarros/sfgpetclinic/model/Vet.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/com/adoniasbarros/sfgpetclinic/model/Vet.java
@@ -1,5 +1,17 @@
 package com.adoniasbarros.sfgpetclinic.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
+
+	private Set<Specialty> specialities;
+
+	public Set<Specialty> getSpecialities() {
+		return specialities;
+	}
+
+	public void setSpecialities(Set<Specialty> specialities) {
+		this.specialities = specialities;
+	}
 
 }
