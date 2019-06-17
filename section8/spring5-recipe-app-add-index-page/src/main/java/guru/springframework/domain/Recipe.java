359632e3d13fa3b8d622c32cdb67dc4705c9166b
adoniasvsbarros <adoniasvicente@hotmail.com>
2019-06-17 20:08:48 -0300

    Creating many to many relationship between Category and Recipe

--- section8/spring5-recipe-app-add-index-page/src/main/java/guru/springframework/domain/Recipe.java
+++ section8/spring5-recipe-app-add-index-page/src/main/java/guru/springframework/domain/Recipe.java
@@ -9,7 +9,10 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.Lob;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
@@ -28,7 +31,6 @@ public class Recipe {
 	private String url;
 	private String directions;
 
-
 	@OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
 	private Set<Ingredient> ingredients;
 
@@ -41,6 +43,11 @@ public class Recipe {
 	@OneToOne(cascade = CascadeType.ALL)
 	private Notes notes;
 
+	@ManyToMany
+	@JoinTable(name = "recipe_category",
+			joinColumns = @JoinColumn(name = "recipe_id"),
+				inverseJoinColumns = @JoinColumn(name = "category_id"))
+	private Set<Category> categories;
 
 	public Long getId() {
 		return id;
@@ -136,6 +143,14 @@ public class Recipe {
 
 	public void setDifficulty(Difficulty difficulty) {
 		this.difficulty = difficulty;
+	}
+
+	public Set<Category> getCategories() {
+		return categories;
+	}
+
+	public void setCategories(Set<Category> categories) {
+		this.categories = categories;
 	}
 
 }
