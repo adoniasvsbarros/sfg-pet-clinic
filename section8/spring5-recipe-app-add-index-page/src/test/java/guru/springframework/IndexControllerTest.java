844fb4e2caefe3280a930a8ed3b3b8d2b251afd1
adoniasvsbarros <adoniasvicente@hotmail.com>
2019-06-24 20:29:12 -0300

    Adding a short controller test with mock mvc

--- section8/spring5-recipe-app-add-index-page/src/test/java/guru/springframework/IndexControllerTest.java
+++ section8/spring5-recipe-app-add-index-page/src/test/java/guru/springframework/IndexControllerTest.java
@@ -4,6 +4,9 @@ import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -15,6 +18,8 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.internal.matchers.Equals;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
 import guru.springframework.controllers.IndexController;
@@ -38,6 +43,15 @@ public class IndexControllerTest {
 		controller = new IndexController(recipeService);
 	}
 
+	@Test
+	public void testMockMVC() throws Exception {
+		MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+		
+		mockMvc.perform(get("/"))
+		.andExpect(status().isOk())
+		.andExpect(view().name("index"));
+	}
+	
 	@Test
 	public void getIndexPage() throws Exception {
 
