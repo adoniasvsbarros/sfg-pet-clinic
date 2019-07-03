fdb8271443c6564fb6f4cdc1ca3762003ab0dc07
adoniasvsbarros <adoniasvicente@hotmail.com>
2019-07-02 21:32:27 -0300

    writing CRUD tests for owner map service, closes #49

--- sfg-pet-clinic/pet-clinic-data/src/main/java/com/adoniasbarros/sfgpetclinic/services/map/OwnerMapService.java
+++ sfg-pet-clinic/pet-clinic-data/src/main/java/com/adoniasbarros/sfgpetclinic/services/map/OwnerMapService.java
@@ -13,12 +13,12 @@ import com.adoniasbarros.sfgpetclinic.services.PetService;
 import com.adoniasbarros.sfgpetclinic.services.PetTypeService;
 
 @Service
-@Profile({"default", "map"})
+@Profile({ "default", "map" })
 public class OwnerMapService extends AbstractMapService<Owner, Long> implements OwnerService {
 
 	private final PetTypeService petTypeService;
 	private final PetService petService;
-	
+
 	@Autowired
 	public OwnerMapService(PetTypeService petTypeService, PetService petService) {
 		this.petTypeService = petTypeService;
@@ -38,22 +38,22 @@ public class OwnerMapService extends AbstractMapService<Owner, Long> implements 
 	@Override
 	public Owner save(Owner object) {
 
-		if(object != null) {
-			if(object.getPets() != null) {
+		if (object != null) {
+			if (object.getPets() != null) {
 				object.getPets().forEach(pet -> {
-					if(pet.getPetType() != null) {
+					if (pet.getPetType() != null) {
 						if (pet.getPetType().getId() == null) {
 							pet.setPetType(petTypeService.save(pet.getPetType()));
 						}
 					} else {
 						throw new RuntimeException("PetType is required");
 					}
-					
-					if(pet.getId() == null) {
+
+					if (pet.getId() == null) {
 						Pet savedPet = petService.save(pet);
 						pet.setId(savedPet.getId());
 					}
-					
+
 				});
 			}
 			return super.save(object);
@@ -74,8 +74,8 @@ public class OwnerMapService extends AbstractMapService<Owner, Long> implements 
 
 	@Override
 	public Owner findByLastName(String lastName) {
-		// TODO Auto-generated method stub
-		return null;
+		return this.findAll().stream().filter(owner -> owner.getLastName().equalsIgnoreCase(lastName)).findFirst()
+				.orElse(null);
 	}
 
 }
