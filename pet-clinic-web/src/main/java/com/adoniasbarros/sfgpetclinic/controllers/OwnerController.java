dc1cd7280fd0448f95d3f37fb121cb9fc883306a
adoniasvsbarros <adoniasvicente@hotmail.com>
2019-07-09 10:16:50 -0300

    Displaying owner record, created tests, controller, views

--- pet-clinic-web/src/main/java/com/adoniasbarros/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/adoniasbarros/sfgpetclinic/controllers/OwnerController.java
@@ -3,7 +3,10 @@ package com.adoniasbarros.sfgpetclinic.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.adoniasbarros.sfgpetclinic.services.OwnerService;
 
@@ -24,8 +27,15 @@ public class OwnerController {
 		return "owners/index";
 	}
 
-	@RequestMapping({ "/find" })
+	@RequestMapping("/find")
 	public String findOwners() {
 		return "notImplemented";
+	}
+	
+	@GetMapping("/{ownerId}")
+	public ModelAndView showOwner(@PathVariable Long ownerId) {
+		ModelAndView mav = new ModelAndView("owners/ownerDetails");
+		mav.addObject(ownerService.findById(ownerId));
+		return mav;
 	}
 }
--- pet-clinic-web/src/test/java/com/adoniasbarros/sfgpetclinic/controllers/OwnerControllerTest.java
+++ pet-clinic-web/src/test/java/com/adoniasbarros/sfgpetclinic/controllers/OwnerControllerTest.java
@@ -2,10 +2,13 @@ package com.adoniasbarros.sfgpetclinic.controllers;
 
 import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.anyLong;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.hamcrest.Matchers.*;
+
 
 import java.util.HashSet;
 import java.util.Set;
@@ -73,6 +76,16 @@ public class OwnerControllerTest {
 		.andExpect(view().name("notImplemented"));
 	
 		verifyZeroInteractions(ownerService);
+	}
+	
+	@Test
+	void displayOwner() throws Exception {
+		when(ownerService.findById(anyLong())).thenReturn(Owner.builder().id(1L).build());
+		
+		mockMvc.perform(get("/owners/123"))
+		.andExpect(status().isOk())
+		.andExpect(view().name("owners/ownerDetails"))
+		.andExpect(model().attribute("owner", hasProperty("id", is(1L))));
 	}
 
 }
